08f6a88a0ce0c56e2704b18503bb3d5cd0b8993e
MoonMinHyuk1 <moon981018@gmail.com>
2022-08-10 03:55:12 +0900

    update

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,21 +14,32 @@ public class JpaMain {
 
         tx.begin();
         try {
+            //비영속
 //            Member member = new Member();
 //            member.setId(2L);
 //            member.setName("HelloB");
+            //영속
 //            em.persist(member);
 
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("HelloJPA");
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(0)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
+
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("=================");
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
 
             tx.commit();
         } catch (Exception e) {
