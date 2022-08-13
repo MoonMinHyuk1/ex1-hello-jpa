211ce268d05391a4de2021cf091e57af0987c1d8
MoonMinHyuk1 <moon981018@gmail.com>
2022-08-14 04:03:26 +0900

    update

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,58 +8,58 @@ import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-
-        tx.begin();
-        try {
-            //비영속
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+//        EntityManager em = emf.createEntityManager();
+//        EntityTransaction tx = em.getTransaction();
+//
+//        tx.begin();
+//        try {
+//            //비영속
+////            Member member = new Member();
+////            member.setId(2L);
+////            member.setName("HelloB");
+//            //영속
+////            em.persist(member);
+//
+////            Member findMember = em.find(Member.class, 1L);
+////            findMember.setName("HelloJPA");
+//
+////            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+////                    .setFirstResult(0)
+////                    .setMaxResults(10)
+////                    .getResultList();
+////            for (Member member : result) {
+////                System.out.println("member.name = " + member.getName());
+////            }
+//
+////            Member member1 = new Member(150L, "A");
+////            Member member2 = new Member(160L, "B");
+////            em.persist(member1);
+////            em.persist(member2);
+////            System.out.println("=================");
+//
+////            Member member = em.find(Member.class, 150L);
+////            em.detach(member); //준영속상태(엔티티 단위)
+////            em.clear(); //영속성 컨텍스트 초기화
+////            em.close(); //영속성 컨텍스트 종료
+////            member.setName("ZZZZZ");
+//
+////            Member member = new Member(200L, "member200");
+////            em.persist(member);
+////            em.flush();
+//
 //            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
-            //영속
+//            member.setUsername("A");
+////            member.setRoleType(RoleType.USER);
+//
 //            em.persist(member);
-
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA");
-
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(0)
-//                    .setMaxResults(10)
-//                    .getResultList();
-//            for (Member member : result) {
-//                System.out.println("member.name = " + member.getName());
-//            }
-
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-//            em.persist(member1);
-//            em.persist(member2);
-//            System.out.println("=================");
-
-//            Member member = em.find(Member.class, 150L);
-//            em.detach(member); //준영속상태(엔티티 단위)
-//            em.clear(); //영속성 컨텍스트 초기화
-//            em.close(); //영속성 컨텍스트 종료
-//            member.setName("ZZZZZ");
-
-//            Member member = new Member(200L, "member200");
-//            em.persist(member);
-//            em.flush();
-
-            Member member = new Member();
-            member.setUsername("A");
-//            member.setRoleType(RoleType.USER);
-
-            em.persist(member);
-
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-            em.close();
-        }
-        emf.close();
+//
+//            tx.commit();
+//        } catch (Exception e) {
+//            tx.rollback();
+//        } finally {
+//            em.close();
+//        }
+//        emf.close();
     }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,100 +5,100 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-@SequenceGenerator(
-        name="member_seq_generator",
-        sequenceName = "member_seq", //매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50
-)
-@Entity
+//@SequenceGenerator(
+//        name="member_seq_generator",
+//        sequenceName = "member_seq", //매핑할 데이터베이스 시퀀스 이름
+//        initialValue = 1, allocationSize = 50
+//)
+//@Entity
 public class Member {
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
-    private Long id;
-
-//    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+//    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
 //    private Long id;
-    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'") //NOT NULL
-    private String username;
-//    private int age;
-//    @Enumerated(EnumType.STRING)
-//    private RoleType roleType;
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date createdDate;
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date lastModifiedDate;
-//    @Lob
-//    private String description;
-//    @Transient
-//    private int temp;
 //
-//    private LocalDate testLocalDate;
-//    private LocalDateTime testLocalDateTime;
-
-    public Member() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-//    public int getAge() {
-//        return age;
+////    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+////    private Long id;
+//    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'") //NOT NULL
+//    private String username;
+////    private int age;
+////    @Enumerated(EnumType.STRING)
+////    private RoleType roleType;
+////    @Temporal(TemporalType.TIMESTAMP)
+////    private Date createdDate;
+////    @Temporal(TemporalType.TIMESTAMP)
+////    private Date lastModifiedDate;
+////    @Lob
+////    private String description;
+////    @Transient
+////    private int temp;
+////
+////    private LocalDate testLocalDate;
+////    private LocalDateTime testLocalDateTime;
+//
+//    public Member() {
+//
 //    }
 //
-//    public void setAge(int age) {
-//        this.age = age;
+//    public Long getId() {
+//        return id;
 //    }
 //
-//    public RoleType getRoleType() {
-//        return roleType;
+//    public void setId(Long id) {
+//        this.id = id;
 //    }
 //
-//    public void setRoleType(RoleType roleType) {
-//        this.roleType = roleType;
+//    public String getUsername() {
+//        return username;
 //    }
 //
-//    public Date getCreatedDate() {
-//        return createdDate;
+//    public void setUsername(String username) {
+//        this.username = username;
 //    }
 //
-//    public void setCreatedDate(Date createdDate) {
-//        this.createdDate = createdDate;
-//    }
-//
-//    public Date getLastModifiedDate() {
-//        return lastModifiedDate;
-//    }
-//
-//    public void setLastModifiedDate(Date lastModifiedDate) {
-//        this.lastModifiedDate = lastModifiedDate;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public int getTemp() {
-//        return temp;
-//    }
-//
-//    public void setTemp(int temp) {
-//        this.temp = temp;
-//    }
+////    public int getAge() {
+////        return age;
+////    }
+////
+////    public void setAge(int age) {
+////        this.age = age;
+////    }
+////
+////    public RoleType getRoleType() {
+////        return roleType;
+////    }
+////
+////    public void setRoleType(RoleType roleType) {
+////        this.roleType = roleType;
+////    }
+////
+////    public Date getCreatedDate() {
+////        return createdDate;
+////    }
+////
+////    public void setCreatedDate(Date createdDate) {
+////        this.createdDate = createdDate;
+////    }
+////
+////    public Date getLastModifiedDate() {
+////        return lastModifiedDate;
+////    }
+////
+////    public void setLastModifiedDate(Date lastModifiedDate) {
+////        this.lastModifiedDate = lastModifiedDate;
+////    }
+////
+////    public String getDescription() {
+////        return description;
+////    }
+////
+////    public void setDescription(String description) {
+////        this.description = description;
+////    }
+////
+////    public int getTemp() {
+////        return temp;
+////    }
+////
+////    public void setTemp(int temp) {
+////        this.temp = temp;
+////    }
 }
