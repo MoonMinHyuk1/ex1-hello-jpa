30c27469bfb5d234c7106242fae99ed951a54c5b
MoonMinHyuk1 <moon981018@gmail.com>
2022-08-14 23:09:06 +0900

    5-2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -28,9 +28,11 @@ public class JpaMain {
             em.clear();
 
             Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
+            List<Member> members = findMember.getTeam().getMembers();
 
-            System.out.println("findTeam = " + findTeam.getName());
+            for(Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
             tx.commit();
 
