89a2fa7ae8f47c471a97f734262eda587ca4fc7f
MoonMinHyuk1 <moon981018@gmail.com>
2022-08-28 23:49:03 +0900

    10-2

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,38 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            List<Member> resultList = query1.getResultList();
+            Member singleResult = query1.getSingleResult(); //결과가 무조건 하나
+
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            TypedQuery<Member> query4 = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                            .setParameter("username", "member1");
+            Member singleResult1 = query4.getSingleResult();
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+}
